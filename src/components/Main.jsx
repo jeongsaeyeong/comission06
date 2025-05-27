758a57522eb7cc670b7cf822f64350c5f02eb615
jeongsaeyeong <ooooo0516@naver.com>
2025-05-27 15:17:43 +0900

    feat: 퍼블리싱 완료

---
+++ src/components/Main.jsx
@@ -0,0 +1,112 @@
+import React from 'react'
+import Start from '../assets/img/A_start.png'
+import img01 from '../assets/img/01_INCHEON.png'
+import img02 from '../assets/img/02_KUALA LUNPUR.png'
+import img03 from '../assets/img/03_BALI.png'
+import img04 from '../assets/img/04_SURABAYA.png'
+import img05 from '../assets/img/05_JAKARTA-1.png'
+import img06 from '../assets/img/06_SINGAPORE.png'
+import img07 from '../assets/img/07_BANGKOK.png'
+import Alone from '../assets/img/B_alone.png'
+import img08 from '../assets/img/08_KAOHSIUNG.png'
+import img09 from '../assets/img/09_HONGKONG.png'
+import img10 from '../assets/img/10_BUSAN.png'
+import img11 from '../assets/img/11_OSAKA.png'
+import img12 from '../assets/img/12_TOKYO.png'
+import Key from '../assets/img/D_key.png'
+import img13 from '../assets/img/13_MANILA.png'
+import img14 from '../assets/img/14_DAEJEON.png'
+import img15 from '../assets/img/15_KWANGJU.png'
+import img16 from '../assets/img/16_DAEGU.png'
+import img17 from '../assets/img/17_SYDNEY.png'
+import img18 from '../assets/img/18_MEBOURNE.png'
+import img19 from '../assets/img/19_AUCKLAND.png'
+import Air from '../assets/img/C_airplane.png'
+import img20 from '../assets/img/20_LA.png'
+import img21 from '../assets/img/21_NEWYORK.png'
+import img22 from '../assets/img/22_YOKOHAMA.png'
+import img23 from '../assets/img/23_JAKARTA.png'
+import img24 from '../assets/img/24_SEOUL.png'
+
+const Main = () => {
+    const top = [
+        { img: Start },
+        { img: img01 },
+        { img: img02 },
+        { img: img03 },
+        { img: img04 },
+        { img: img05 },
+        { img: img06 },
+        { img: img07 },
+        { img: Alone },
+    ]
+
+    const right = [
+        { img: img08 },
+        { img: img09 },
+        { img: img10 },
+        { img: img11 },
+        { img: img12 },
+    ]
+
+    const left = [
+        { img: img24 },
+        { img: img23 },
+        { img: img22 },
+        { img: img21 },
+        { img: img20 },
+    ]
+
+    const bottom = [
+        { img: Key },
+        { img: img19 },
+        { img: img18 },
+        { img: img17 },
+        { img: img16 },
+        { img: img15 },
+        { img: img14 },
+        { img: img13 },
+        { img: Air },
+    ]
+
+    return (
+        <div className='Main_wrap'>
+            <div className="box">
+                <div className="first">
+                    {top.map((item, key) => (
+                        <div className="img-wrapper" key={key}>
+                            <img src={item.img} alt="" />
+                        </div>
+                    ))}
+                </div>
+
+                <div className="center">
+                    <div className="left">
+                        {left.map((item, key) => (
+                            <div className="img-wrapper" key={key}>
+                                <img src={item.img} alt="" />
+                            </div>
+                        ))}
+                    </div>
+                    <div className="right">
+                        {right.map((item, key) => (
+                            <div className="img-wrapper" key={key}>
+                                <img src={item.img} alt="" />
+                            </div>
+                        ))}
+                    </div>
+                </div>
+
+                <div className="bottom">
+                    {bottom.map((item, key) => (
+                        <div className="img-wrapper" key={key}>
+                            <img src={item.img} alt="" />
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Main
